a01b192a635ba821f95f26cf0eb23c8397815ed0
RikDuijm <rikduijm@gmail.com>
2020-07-15 10:31:39 +0000

    setup allauth

--- travel/urls.py
+++ travel/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('naturandes/', include('naturandes.urls')),
     path('admin/', admin.site.urls),
-    
+    path('accounts/', include('allauth.urls')),
 ]
 
 
