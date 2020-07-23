912f763cbe439b1bf664a2022e1a795e0ae931c7
RikDuijm <rikduijm@gmail.com>
2020-07-23 13:36:39 +0000

    created second customer

--- customers/views.py
+++ customers/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+
+from .models import Customer
 
 
 def index(request):
     """ Display the customer's page"""
 
     if request.user.is_authenticated:
+        # user is indentified by his email
+        # user = User.objects.get(email=request.user.email)
 
-        template = 'customer/customer.html'
-        context = {}
+        profile = get_object_or_404(Customer, user=request.user)
+
+        if request.user == 'hoii':
+            template = 'customer/customer.html'
+            context = {
+                'profile': profile,
+            }
+        else:
+            template = 'customer/customer1.html'
+            context = {
+                'profile': profile,
+            }
 
         return render(request, template, context)
 
